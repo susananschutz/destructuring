58771534c848d3c12ac9ea6114835a025a90ba77
susananshcutz <drsusananschutz@gmail.com>
2022-09-05 20:40:03 -0500

    homework

---
+++ destructuring.js
@@ -0,0 +1,81 @@
+let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
+let {numPlanets, yearNeptuneDiscovered} = facts;
+
+console.log(numPlanets); 8
+console.log(yearNeptuneDiscovered); 1846
+
+
+
+let planetFacts = {
+    numPlanets: 8,
+    yearNeptuneDiscovered: 1846,
+    yearMarsDiscovered: 1659
+  };
+  
+  let {numPlanets, ...discoveryYears} = planetFacts;
+  
+  console.log(discoveryYears); // ?
+
+yearMarsDiscovered: 1659
+yearNeptuneDiscovered: 1846
+[[Prototype]]: Object
+
+function getUserData({firstName, favoriteColor="green"}){
+    return `Your name is ${firstName} and you like ${favoriteColor}`;
+  }
+  getUserData({firstName: "Alejandro", favoriteColor: "purple"})
+  'Your name is Alejandro and you like purple'
+  getUserData({firstName: "Melissa"}) // ?
+  Your name is Melissa and you like green'
+  getUserData({}) // ?
+  'Your name is undefined and you like green'
+
+let [first, second, third] = ["Maya", "Marisa", "Chi"];
+
+  console.log(first); "Maya"
+console.log(second); "Marissa"
+console.log(third); "Chi"
+
+let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
+    "Raindrops on roses",
+    "whiskers on kittens",
+    "Bright copper kettles",
+    "warm woolen mittens",
+    "Brown paper packages tied up with strings"
+  ]
+  
+  console.log(raindrops); "Raindrops on roses"
+  console.log(whiskers); "whiskers on kittens"
+  console.log(aFewOfMyFavoriteThings); "Bright copper kettles", "warm woolen mittens", "Brown paper packages tied up with string"
+
+  let numbers = [10, 20, 30];
+  [numbers[1], numbers[2]] = [numbers[2], numbers[1]]
+  
+  console.log(numbers) 3) [10, 30, 20]
+
+ assigning variables to object properties
+ var obj = {
+    numbers: {
+      a: 1,
+      b: 2
+    }
+  };
+  
+  var a = obj.numbers.a;
+  var b = obj.numbers.b;
+  const {a,b} = obj.numbers;
+
+  
+  Array swap
+   var arr = [1, 2];
+var temp = arr[0];
+arr[0] = arr[1];
+arr[1] = temp;
+  
+[arr[0], arr[1]] = [arr[1], arr[0]]
+
+const Results = ['Tom', 'Margaret', 'Allison', 'David', 'Pierre'];
+function raceResults(first,second,third,...rest){
+    return {first, second, third, rest};
+}
+const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest})
